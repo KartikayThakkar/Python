d93cdd681991ec5bfff7027fef7e86ebfd9f0e99
KartikayThakkar <kartikaythakkar@gmail.com>
2017-11-12 11:03:44 +0530

    Add files via upload

---
+++ Strings/Capitalize!.py
@@ -0,0 +1,10 @@
+"""Capitalize each word of a string
+
+
+Input:hello world
+Output:Hello World
+
+We can use .title() bcoz 123gas.title() will result out 123Gas.but we want its ans to be 123gas.bcoz "1" is the first character in it
+str=string.title() WRONG
+
+"""
